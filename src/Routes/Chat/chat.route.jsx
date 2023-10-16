26cade3028b1756a65e311dffee9098986f6ee72
Harsimran Kaur <infamously13@gmail.com>
2023-10-16 17:45:37 -0400

    Removing userRoom from chat

--- src/Routes/Chat/chat.route.jsx
+++ src/Routes/Chat/chat.route.jsx
@@ -2,38 +2,38 @@ import React, { Fragment, useEffect, useState, useRef, useContext } from "react"
 import './chat.styles.css'
 import Message from "../../Component/Message/message.component";
 import Input from "../../Component/Input/input.component";
-import { UserRoomContext } from "../../Context/userRoom.context";
+// import { UserRoomContext } from "../../Context/userRoom.context";
 
 //Firestore
-import { db } from "../../Utility/firebase.utility";
-import {collection, limit, onSnapshot, orderBy, query, where } from "firebase/firestore";
+// import { db } from "../../Utility/firebase.utility";
+// import {collection, limit, onSnapshot, orderBy, query, where } from "firebase/firestore";
 
 const Chat = () => {
     const [messages, setMessages] = useState([]);
-    const {currentRoom} = useContext(UserRoomContext)
+    // const {currentRoom} = useContext(UserRoomContext)
     const scroll = useRef();
     
-    useEffect(() => {
-        console.log("currentRoom before query ", currentRoom);
-        const q = query(
-            collection(db, "messages"),
-            where("room", "==", currentRoom),
-            orderBy("createdAt", "desc"),
-            limit(50)
-        )
+    // useEffect(() => {
+    //     // console.log("currentRoom before query ", currentRoom);
+    //     const q = query(
+    //         collection(db, "messages"),
+    //         where("room", "==", currentRoom),
+    //         orderBy("createdAt", "desc"),
+    //         limit(50)
+    //     )
 
-        const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-            const fetchedMessages = [];
-            QuerySnapshot.forEach((doc) => {
-                fetchedMessages.push({...doc.data(), id:doc.id});
-            });
-            const sortedMessage = fetchedMessages.sort(
-                (a,b) => a.createdAt - b.createdAt
-            );
-            setMessages(sortedMessage);
-        });
-        return () => unsubscribe;
-    }, []);
+    //     const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+    //         const fetchedMessages = [];
+    //         QuerySnapshot.forEach((doc) => {
+    //             fetchedMessages.push({...doc.data(), id:doc.id});
+    //         });
+    //         const sortedMessage = fetchedMessages.sort(
+    //             (a,b) => a.createdAt - b.createdAt
+    //         );
+    //         setMessages(sortedMessage);
+    //     });
+    //     return () => unsubscribe;
+    // }, []);
 
 
     return (
